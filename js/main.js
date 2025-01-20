562f8524c84323e45d2ffe7cb200e68681d3832d
Selena Pham <selena40253@gmail.com>
2025-01-19 22:45:42 -0800

    Implemented the random button.

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const outputArea = document.querySelector('.js-output-area');
+
+// Load Gif functions
+function renderGif(gif) {
+    return `
+        <img src='${gif.data.images.original.url}' alt'${gif.data.title}' />
+    `;
+}
+
+function displayGifs(gifList) {
+    outputArea.innerHTML = gifList
+        .filter(gif => gif.meta.status === 200) // removes gifs that could not be retrieved
+        .map(gif => renderGif(gif))
+        .join('');
+}
+
+// Random Gif functions
+function fetchRandomGif() {
+    return fetch('https://api.giphy.com/v1/gifs/random?api_key=YmjDpGYsDIWqYVvReohWVgkQPv2Bgmnh');
+}
+
+function generateRandomGif() {
+    fetchRandomGif()
+        .then(data => data.json())
+        .then(response => displayGifs([response])); // input for displayGifs must be an array to use the filter function
+}
